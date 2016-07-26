949b2d6eefaa165bbf6e8f7f949dab78364d5c0e
Vanja Ruzic <javruzic@gmail.com>
2016-07-26 14:16:22 +0200

    page 2

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 $(document).ready(function () {
 
-  $("#owl-example").owlCarousel();
- 
+    $("#owl-example").owlCarousel({
+        navigation: true,
+        navigationText: ["prev", "next"],
+    });
+
+    $("a.down").click(function () {
+        $(this).next('ul').slideToggle();
+        $(this).children().toggleClass("fa-toggle-up");
+    });
+
+    $(function () {
+        $("#slider-range").slider({
+            range: true,
+            min: 0,
+            max: 500,
+            values: [75, 300],
+            slide: function (event, ui) {
+                $("#amount").val("$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ]);
+            }
+        });
+        $("#amount").val("$" + $("#slider-range").slider("values", 0) +
+                " - $" + $("#slider-range").slider("values", 1));
+    });
+
 });
