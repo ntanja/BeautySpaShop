aceedfb97ab5527121630fc394fef1896ff74c7b
Tanja Nikolic <tanja.nikolic.rs@gmail.com>
2016-07-28 14:09:45 +0200

    page 2, 3, 4

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
-
+//owl carousel
     $("#owl-example").owlCarousel({
         navigation: true,
         navigationText: ["prev", "next"],
     });
+    
 
     $("a.down").click(function () {
         $(this).next('ul').slideToggle();
         $(this).children().toggleClass("fa-toggle-up");
     });
-
+    
+//price range slider 
     $(function () {
         $("#slider-range").slider({
             range: true,
@@ -23,5 +25,58 @@ $(document).ready(function () {
         $("#amount").val("$" + $("#slider-range").slider("values", 0) +
                 " - $" + $("#slider-range").slider("values", 1));
     });
-
+    //
+    $("article.preview nav").show();
+	var previewImg = $("img#main");
+	$("a:first").addClass("active");
+	$(".preview nav a img").click(function(){
+		var link = $(this).parent();	
+		var linkHref = link.attr("href");			
+		var linkAlt = link.attr("alt");			
+		if( $(link).hasClass("active") == false)
+		{
+			$("a").removeClass("active");
+			link.addClass("active");											
+			$(previewImg).animate({
+				opacity: 0.8,
+			}, 300, function() {
+				previewImg.attr("src", linkHref);
+				previewImg.attr("alt", linkAlt);				
+				$(this).animate({
+					opacity: 1,
+					}, 300
+				);							
+			});			
+		}
+		return false;
+	});
+	$("input").click(function(){
+		$("p.more").fadeIn("slow");
+	});
+        
+  //spinner 
+  $( function() {
+    var spinner = $( "#spinner" ).spinner();
+ 
+    $( "#disable" ).on( "click", function() {
+      if ( spinner.spinner( "option", "disabled" ) ) {
+        spinner.spinner( "enable" );
+      } else {
+        spinner.spinner( "disable" );
+      }
+    });
+    $( "#destroy" ).on( "click", function() {
+      if ( spinner.spinner( "instance" ) ) {
+        spinner.spinner( "destroy" );
+      } else {
+        spinner.spinner();
+      }
+    });
+    $( "button" ).button();
+  } );
+       
+ //tabulator 
+   $( function() {
+    $( "#tabs" ).tabs();
+  });
 });
